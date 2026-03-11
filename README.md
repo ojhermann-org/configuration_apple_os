f0a5fbd66d23506e30e18070e49d225ac441ff42
Otto J. Hermann <ojhermann@gmail.com>
2026-03-11 18:21:42 -0400

    v0 (#1)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # configuration_apple_os
-Something I can run on any Apple machine to have a working machine
+
+## what
+
+[Zsh](https://www.zsh.org/) scripting to get an Apple machine set up for basic development
+
+## why
+
+I need something that works for an individual user on an arbitrary Apple machine; basic `Zsh` scripting provides this on even under very stringent security set ups; I would prefer to use `Nix` like I do on [EC2 instances](https://github.com/ojhermann/nix-os-ec2)
+
+## how
+
+I've created some `Zsh` scripts to install an configure a few applications, scripts, and related matters; to use this repo, copy it to the `Documents` folder on your Apple machine and then execute `main.sh`
---
+++ main.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env zsh
+
+function install-apps {
+	source ~/Documents/configuration_apple_os/apps/brew/install.sh
+
+	for dir in ~/Documents/configuration_apple_os/apps/*; do
+		if [[ -d "$dir" && "$(basename "$dir")" != "brew" ]]; then
+			for file in "$dir"/install.sh; do
+				source "$file"
+			done
+		fi
+	done
+}
+
+function setup-scripts {
+	for file in ~/Documents/configuration_apple_os/scripts/*sh; do
+		if [[ -f "$file" ]]; then
+			BF="$(basename "$file")"
+			F=${BF%.sh}
+			P="/usr/bin/$F"
+			if [[ ! -e "$P" ]]; then
+				ln -s "$file" "/usr/bin/$F"
+			fi
+		fi
+	done
+}
+
+function setup-aliases {
+	if [[ ! -f "~/.zshrc" ]]; then
+		touch ~/.zshrc
+	fi
+
+	COMMAND="source ~/Documents/configuration_apple_os/aliases.sh"
+	if ! grep -Fxq "$COMMAND" "~/.zshrc"; then
+		echo $COMMAND >>"~/.zshrc"
+	fi
+}
+
+install-apps
+setup-scripts
+setup-setup-aliases
+PROMPT="%F{yellow}%n@%m in %~ > %f"
+my-rpromt
