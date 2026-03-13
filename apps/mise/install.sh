2cfc789b5e4fbb48d18fe8df777175cfc1d6f7c1
Otto Hermann <ojhermann@gmail.com>
2026-03-13 16:21:51 -0400

    update mise install.sh

--- apps/mise/install.sh
+++ apps/mise/install.sh
@@ -6,6 +6,7 @@ print -P "%F{green}%B$APP%b: starting to install $APP%f"
 
 if ! command -v mise &>/dev/null; then
 	curl https://mise.run | sh
+	echo "eval \"\$(~/.local/bin/mise activate zsh)\"" >>~/.zshrc
 fi
 
 print -P "%F{green}%B$APP%b: $APP has been installed%f"
