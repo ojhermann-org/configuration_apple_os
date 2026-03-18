603ce6352b8b032b95960084d50b215ce55202c8
Otto Hermann <ojhermann@gmail.com>
2026-03-18 17:35:25 -0400

    watch-dir.sh

---
+++ scripts/watch-dir.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+if git rev-parse --git-dir >/dev/null 2>&1; then
+	watchexec -c --watch "." --watch ".git/index" --watch ".git/HEAD" "--no-vcs-ignore" -i ".git/objects/**" -i ".git/logs/**" gst
+else
+	watchexec -c --watch "." gst
+fi
