912701aa7af51f5e29a73dfda0112a4de5815f10
Otto Hermann <ojhermann@gmail.com>
2026-03-20 19:32:02 -0400

    home-manager

--- apps/nix/install.sh
+++ apps/nix/install.sh
@@ -9,4 +9,8 @@ if ! command -v nix &>/dev/null; then
 	curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 fi
 
+if [ ! -f ~/.config/home-manager/flake.nix ]; then
+	nix run home-manager/master -- init --switch
+fi
+
 print -P "%F{green}%B$APP%b: $APP has been installed%f"
